4d4156f0c4bc0104c91f4bfd89cd586cd3df9c90
Luvasc <lucasgdcarrizo@gmail.com>
2025-11-29 14:41:50 -0300

    agregando comentarios prodctos

--- src/productos.ts
+++ src/productos.ts
@@ -15,3 +15,49 @@ export interface Categoria {
 
  export const productos: Producto[] = [
 ];
+
+
+
+
+
+/*
+{
+  "id": 1,
+  "nombre": "Mouse Gamer",
+  "precio": 15999,
+  "disponible": true,
+  "categorias": [
+    {
+      "id": 10,
+      "nombre": "Periféricos",
+      "stock": 120
+    },
+    {
+      "id": 11,
+      "nombre": "Gaming",
+      "stock": 80
+    }
+  ]
+}
+
+
+
+
+
+{
+  "id": 2,
+  "nombre": "Teclado Mecánico",
+  "precio": 29999,
+  "disponible": false,
+  "categorias": [
+    {
+      "id": 20,
+      "nombre": "Teclados",
+      "stock": 50
+    }
+  ]
+}
+
+
+
+*/
